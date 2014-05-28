3fbc34c101b76d2c6cb813c5657c3198381bb569
Qiu Juntao <juntao.qiu@gmail.com>
2014-05-28 13:37:41 +1000

    add more real data, by Sara

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'json'
 
+set :bind, "0.0.0.0"
+
 get '/kpi/:type' do
     kpi = File.read(File.open("data/#{params[:type]}.json"))
     erb :index, :locals => {:kpi => kpi}
