e6f463b57415c03756c9bad81bd189be64901355
Qiu Juntao <juntao.qiu@gmail.com>
2014-05-29 23:11:50 +1000

    move static json to amazon s3

--- app.rb
+++ app.rb
@@ -1,59 +1,24 @@
 require 'sinatra'
 require 'json'
 
+require 'net/http'
+require 'uri'
+
+require './lib/kpi'
+
 set :bind, "0.0.0.0"
+
+
+use Rack::Auth::Basic do |username, password|
+    username == 'admin' && password == 'admin'
+end
 
 get '/' do
     redirect '/kpi/psr'
 end
 
 get '/kpi/:type' do
-    kpi = File.read(File.open("data/#{params[:type]}.json"))
+    kpi = Net::HTTP.get(URI.parse("https://s3.amazonaws.com/kpis/#{params[:type]}.json"))
     erb :index, :locals => {:kpi => kpi, :type => params[:type]}
-end
-
-get '/admin' do
-    now = Time.now.strftime("%m/%d/%Y")
-    erb :admin, :locals => {:today => now}
-end
-
-get '/upload' do
-    erb :upload
-end
-
-post '/uploadkpi' do
-  unless params[:file] &&
-         (tmpfile = params[:file][:tempfile]) &&
-         (name = params[:file][:filename])
-      halt
-  end
-
-  str = ""
-  while blk = tmpfile.read(4096)
-    str += blk
-  end
-
-  File.open("data/#{name}", 'w') do |f|
-      f.write(str)
-  end
-
-  type = name.chomp(File.extname(name))
-  redirect "/kpi/#{type}"
-end
-
-post '/updatefig' do
-    date = params[:date]
-    psr = params[:psr]
-    ccr = params[:ccr]
-    lusr = params[:lusr]
-    asr2g = params[:asr2g]
-    asr3g = params[:asr3g]
-    pdpsr2g = params[:pdpsr2g]
-    pdpsr3g = params[:pdpsr3g]
-    hosr = params[:hosr]
-    ccsrcs = params[:ccsrcs]
-    ccsrps = params[:ccsrps]
-
-    p date, psr
 end
 
