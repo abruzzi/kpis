d8fbcf284e838871f7b171922c9d28a0f30c824f
Qiu Juntao <juntao.qiu@gmail.com>
2014-06-02 12:38:55 +1000

    ugly fix the multiple plots in one chart

--- app.rb
+++ app.rb
@@ -12,8 +12,28 @@ get '/' do
     redirect '/kpi/psr'
 end
 
+def parse
+end
+
 get '/kpi/:type' do
-    kpi = Net::HTTP.get(URI.parse("http://s3.amazonaws.com/kpis/#{params[:type]}.json"))
-    erb :index, :locals => {:kpi => kpi, :type => params[:type]}
+    type = params[:type].downcase
+
+    if(['asr', 'pdpsr'].include? type) 
+        kpi2g = JSON.parse(Net::HTTP.get(URI.parse("http://s3.amazonaws.com/kpis/#{type}2g.json")))
+        kpi3g = JSON.parse(Net::HTTP.get(URI.parse("http://s3.amazonaws.com/kpis/#{type}3g.json")))
+
+        kpis = {:data => [], 
+                :title => [kpi2g["title"], kpi3g["title"]], 
+                :target => kpi2g["target"],
+                :negativeColor => ["red", "red"],
+                :color => ["green", "blue"]
+        }
+        kpis[:data] << kpi2g["data"]
+        kpis[:data] << kpi3g["data"]
+    else
+        kpis = JSON.parse(Net::HTTP.get(URI.parse("http://s3.amazonaws.com/kpis/#{type}.json")))
+    end
+    
+    erb :index, :locals => {:kpis => kpis.to_json, :type => type}
 end
 
